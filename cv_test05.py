d74ef6461cb603e77ef1e4a7ca99363442332af8
kurodebian <kuro_debi_an@yahoo.co.jp>
2017-11-06 17:00:46 +0000

    second commit

--- cv_test05.py
+++ cv_test05.py
@@ -1,10 +1,10 @@
 import cv2
 
 def main():
-    img = cv2.imread( 'messi5.jpg', cv2.IMREAD_COLOR )
+    img = cv2.imread( 'apple.jpg', cv2.IMREAD_COLOR )
 
     out = cv2.cvtColor( img, cv2.COLOR_BGR2HSV )
-    cv2.imwrite("gray1.jpg", out)
+    cv2.imwrite("gray2.jpg", out)
 
 if __name__ == "__main__":
     main()
